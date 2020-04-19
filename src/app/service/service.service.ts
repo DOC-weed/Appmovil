1d3b0c81a027a5412bf4a85745232f5e24051579
Adrian <rafael.avendano12317@gmail.com>
2020-04-19 00:26:08 -0500

    Agregando todas las rutas

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -23,29 +23,65 @@ export class ServiceService {
 
 //crear usuario
   postUser(User: user){
-    return this.http.post(this.urlUsers + '/', User);
+    return this.http.post(this.urlUsers + '/', User).toPromise();
+  }
+
+  getUsers(){
+      return this.http.get(this.urlUsers + '/').toPromise();
+  }
+
+  getUserId(_id:any){
+    return this.http.get(this.urlUsers + `/${_id}`).toPromise();
+  }
+
+
+  updateUser(_id:any,User: user){
+    return this.http.post(this.urlUsers +  `/${_id}`,User).toPromise();
+  }
+  updateUser(_id:any,img:string){
+    return this.http.post(this.urlUsers + `/${_id}/${img}`).toPromise();
   }
 
 //crear cursos
   postCourse(Courseinfo: Course){
-    return this.http.post(this.urlCourses , Courseinfo);
+    return this.http.post(this.urlCourses , Courseinfo).toPromise();
   }
 
 //Obtener curso por id
-  GetCourseId(_id: string){
-      return this.http.get(this.urlCourses + `/${_id}`);
+  GetCourseId(_id: any){
+      return this.http.get(this.urlCourses + `/${_id}`).toPromise();
+  }
+
+  GetCourseIdP(_id: any){
+      return this.http.get(this.urlCourses + `Persona/${_id}`).toPromise();
+  }
+
+  getCourse(){
+    return this.http.get(this.urlCourses ).toPromise();
+  }
+
+  updateCourse(_id:any,Courseinfo:Course){
+    return this.http.put(this.urlCourses + `/${_id}`,Courseinfo).toPromise();
+  }
+
+  updateCourseMsg(_id:any,msg:string){
+    return this.http.put(this.urlCourses + `/${_id}/${msg}`).toPromise();
   }
 
   //crear homework
   postHomework(Homework: homework){
     console.log(Homework);
 
-    return this.http.post(this.urlHomeworks , Homework);
+    return this.http.post(this.urlHomeworks , Homework).toPromise();
 
   }
 //Obtener homework por id
   GetHomeworkId(_id: string){
-        return this.http.get(this.urlHomeworks + `/${_id}`);
+        return this.http.get(this.urlHomeworks + `/${_id}`).toPromise();
+  }
+
+  getHomeworks(){
+    return this.http.get(this.urlHomeworks).toPromise();
   }
 
 }
